cf46304aead4936688457cfd1416a8dfd19a0640
Davin Haley <dhaley4@student.cvtc.edu>
2023-02-15 16:37:16 -0600

    implement newFolderName

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
 const newFolderName = function (folders) {
 
+  let folderName = 'New Folder'
+  let folderNumber = 1
+  while (folders.includes(folderName)) {
+    folderName = `New Folder (${folderNumber})`
+    folderNumber++
+  }
+
+  console.log(folderName) // can replace with return statement if necessary
+
 }
+
+newFolderName([])
+newFolderName(["New Folder"])
+newFolderName(['stinky', 'New Folder', 'New Folder (1)', 'New Folder (2)'])
