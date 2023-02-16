9d8f8da16958455019bbefb7c6e14e93cc851bfb
Davin Haley <dhaley4@student.cvtc.edu>
2023-02-16 11:24:34 -0600

    add Jasmine and nyc

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
 const newFolderName = function (folders) {
 
   let folderName = 'New Folder'
-  let folderNumber = 1
+  let folderNumber = 2
   while (folders.includes(folderName)) {
     folderName = `New Folder (${folderNumber})`
     folderNumber++
   }
 
-  console.log(folderName) // can replace with return statement if necessary
+  return folderName
 
 }
 
-newFolderName([])
-newFolderName(["New Folder"])
-newFolderName(['stinky', 'New Folder', 'New Folder (1)', 'New Folder (2)'])
+module.exports = { newFolderName }
